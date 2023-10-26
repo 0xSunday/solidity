875a560fe59ef33a42a4ef705e0a4debbac23416
Sunil Reddy <sunday7637@gmail.com>
2023-10-26 12:56:34 +0530

    storage factory

---
+++ StorageFactory.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public simpleStorageArray;
+
+    function createNewSimpleStorage() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    //to use store function inside the simple storage
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber)
+        public
+    {
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+    }
+}
